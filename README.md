09edcb780edaf309c5c7f68735d59cfa90c4731d
Melinda Zhu <mzhu@Melindas-MBP.hsd1.ca.comcast.net>
2023-11-25 02:05:44 -0800

    setup init env with Gym class framework

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# AA228_SquidGameRL
+# AA228 Final Project - Squid Game Glass Bridge w/ RL & Probabilistic Reasoning
+
+## Prerequisites
+1. Clone this repository: `$ git clone https://github.com/melindazhu/AA228_SquidGameRL.git`
+2. Set up a Python virtual environment. We do this because package versions were specifically compatible with the packages listed in requirements.txt. Run the following: 
+    - `$ pip install virtualenv` in home directory
+    - `$ cd /path/to/this/repo`
+    - `$ virtualenv venv`
+    - `$ source venv/bin/activate` for Mac and `venv\Scripts\activate` for Windows
+3. Install packages required for this repo (WIP): `$ pip install -r requirements.txt`
+
+
+## Part 1: Creating an RL agent <br>
+Formulate the problem as a Markov Decision Process: 
+- **agent** = player
+- **environment** = the glass bridge, defined as a grid
+- **reward** = 1 if whole bridge is traversed, 0 otherwise
+- **action** = moving to next adjacent position or the far diagonal position
+- **state** = position of the agent on the glass bridge grid
---
+++ reinforcement_learning/glass_bridge.py
@@ -0,0 +1,110 @@
+import gym
+from gym.envs.toy_text import discrete
+import numpy as np
+import sys
+
+
+ACTION_RIGHT = 0
+ACTION_DIAGONAL = 1
+
+MAP = {
+    "5x2": ["SO--G", "--OO-"]
+}
+
+class GlassBridgeEnv(discrete.DiscreteEnv):
+    """
+    A GlassBridgeEnv object defines the state space of the MDP.
+    The letter indicators are as follows: 
+    S : starting point, safe
+    - : thin glass, fail
+    O : tempered glass, safe
+    G : goal -> reward = 1 if reached
+
+    Follows the class framework of OpenAI Gym environments.
+    """
+    def __init__(self):
+        bridge_map = MAP["5x2"]
+        self.bridge_map = bridge_map = np.asarray(bridge_map, dtype="c")
+        self.n_rows, self.n_cols = n_rows, n_cols = bridge_map.shape
+        self.possible_rewards = (0, 1)
+
+        n_actions = 2
+        n_states = n_rows * n_cols
+        
+        initialize_states = np.array(bridge_map == b"S").astype("float64").ravel()
+        initialize_states /= initialize_states.sum()
+
+        # define transition probabilities
+        # T[s][a] is a list of tuples representing the possible transitions from state `s` with action `a`.
+        # Each tuple contains (probability, s_prime, reward, done). 
+        T = {s: {a: [] for a in range(n_actions)} for s in range(n_states)}
+
+        # return the state (i.e. position on the bridge) specified by (row, col)
+        def to_s(row, col):
+            return row * n_rows + col
+
+        # return the new (rol, col) after an action is performed
+        def inc(row, col, action):
+            col = min(col + 1, n_cols - 1)
+            if action == ACTION_DIAGONAL:
+                if row == 0:
+                    row = 1
+                elif row == 1:
+                    row = 0
+            return (row, col)
+
+        # returns (newstate, reward, done)
+        def update_probability_matrix(row, col, action):
+            next_row, next_col = inc(row, col, action)
+            s_prime = to_s(next_row, next_col)
+            new_letter = bridge_map[next_row, next_col]
+            done = bytes(new_letter) in b"G-"
+            r = float(new_letter == b"G")
+            return s_prime, r, done
+        
+        for row in range(n_rows):
+            for col in range(n_cols):
+                s = to_s(row, col)
+                for a in range(n_actions): # only two possible actions
+                    known_transitions = T[s][a]
+                    curr_letter = bridge_map[row, col]
+                    if curr_letter in b"G-": # reached the end of bridge
+                        known_transitions.append((1.0, s, 0, True)) # (prob, s_prime, r, done)
+                    else:
+                        known_transitions.append((1.0, *update_probability_matrix(row, col, a)))
+        print(f"known transitions: {known_transitions}")
+        super(GlassBridgeEnv, self).__init__(n_states, n_actions, T, initialize_states)
+
+    def render(self):
+        outfile = sys.stdout
+        row, col = self.s // self.n_cols, self.s % self.n_cols
+        bridge_map = self.bridge_map.tolist()
+        bridge_map = [[c.decode("utf-8") for c in line] for line in bridge_map]
+        bridge_map[row][col] = gym.utils.colorize(bridge_map[row][col], "red", highlight=True)
+        if self.lastaction is not None:
+            outfile.write(
+                "  ({})\n".format(["Right", "Diagonal"][self.lastaction])
+            )
+        else:
+            outfile.write("\n")
+        outfile.write("\n".join("".join(line) for line in bridge_map) + "\n")
+    
+
+def main(): 
+    # this works for one fixed-size grid, WILL CHANGE LATER (11/25 mz)
+    env = GlassBridgeEnv()
+    for i_episode in range(3): # number of trials we want to try the game
+        newstate = env.reset()
+        for t in range(10):
+            env.render()
+            print(newstate)
+            action = env.action_space.sample()
+            newstate, reward, done, info = env.step(action)
+            if done:
+                print("Trial finished after {} timesteps".format(t+1))
+                break
+    env.close()
+
+
+if __name__ == "__main__":
+    main()
